c23909b862729962a432842d100aa4a78083e0a1
VoQuocTinh <133622416+VoQuocTinh@users.noreply.github.com>
2023-12-06 16:15:33 +0700

    register

--- MyFoodApp/app/src/main/java/com/edu/voquoctinh/myfoodapp/WelcomActivity.java
+++ MyFoodApp/app/src/main/java/com/edu/voquoctinh/myfoodapp/WelcomActivity.java
@@ -1,14 +1,26 @@
 package com.edu.voquoctinh.myfoodapp;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class WelcomActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_welcom);
+    }
+
+    public void register(View view) {
+        startActivity(new Intent(WelcomActivity.this, RegistrationActivity.class));
     }
 }

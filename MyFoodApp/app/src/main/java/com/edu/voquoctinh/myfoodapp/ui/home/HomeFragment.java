0afdc17b105ce6ae9eb4560afbea19e840505ccf
VoQuocTinh <133622416+VoQuocTinh@users.noreply.github.com>
2023-12-10 20:49:39 +0700

    update

--- MyFoodApp/app/src/main/java/com/edu/voquoctinh/myfoodapp/ui/home/HomeFragment.java
+++ MyFoodApp/app/src/main/java/com/edu/voquoctinh/myfoodapp/ui/home/HomeFragment.java
@@ -9,10 +9,22 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.edu.voquoctinh.myfoodapp.R;
+import com.edu.voquoctinh.myfoodapp.adapters.HomeHorAdapter;
 import com.edu.voquoctinh.myfoodapp.databinding.FragmentHomeBinding;
+import com.edu.voquoctinh.myfoodapp.models.HomeHorModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
+
+    RecyclerView homeHorizontalRec;
+    List<HomeHorModel> homeHorModelList;
+    HomeHorAdapter homeHorAdapter;
 
     private FragmentHomeBinding binding;
 
@@ -24,9 +36,26 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
+        final TextView textView = binding.editText2;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        homeHorizontalRec = root.findViewById(R.id.home_hor_rec);
+
+        homeHorModelList = new ArrayList<>();
+
+        homeHorModelList.add(new HomeHorModel(R.drawable.pizza,"Pizza"));
+        homeHorModelList.add(new HomeHorModel(R.drawable.hamburger,"Hamburger"));
+        homeHorModelList.add(new HomeHorModel(R.drawable.khoaitaychien,"Fries"));
+        homeHorModelList.add(new HomeHorModel(R.drawable.ice_cream,"Ice Cream"));
+        homeHorModelList.add(new HomeHorModel(R.drawable.sandwich,"Sandwich"));
+
+        homeHorAdapter = new HomeHorAdapter(getActivity(),homeHorModelList);
+        homeHorizontalRec.setAdapter(homeHorAdapter);
+        homeHorizontalRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        homeHorizontalRec.setHasFixedSize(true);
+        homeHorizontalRec.setNestedScrollingEnabled(false);
         return root;
+
     }
 
     @Override

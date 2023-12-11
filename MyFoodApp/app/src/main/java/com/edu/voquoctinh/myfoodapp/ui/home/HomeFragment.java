746f99ea34d2c488fa4974a6859aba79a674ad9f
VoQuocTinh <133622416+VoQuocTinh@users.noreply.github.com>
2023-12-11 22:36:41 +0700

    update 11/12/2023

--- MyFoodApp/app/src/main/java/com/edu/voquoctinh/myfoodapp/ui/home/HomeFragment.java
+++ MyFoodApp/app/src/main/java/com/edu/voquoctinh/myfoodapp/ui/home/HomeFragment.java
@@ -14,17 +14,24 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.edu.voquoctinh.myfoodapp.R;
 import com.edu.voquoctinh.myfoodapp.adapters.HomeHorAdapter;
+import com.edu.voquoctinh.myfoodapp.adapters.HomeVerAdapter;
 import com.edu.voquoctinh.myfoodapp.databinding.FragmentHomeBinding;
 import com.edu.voquoctinh.myfoodapp.models.HomeHorModel;
+import com.edu.voquoctinh.myfoodapp.models.HomeVerModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-    RecyclerView homeHorizontalRec;
+    RecyclerView homeHorizontalRec, homeVerticalRec;
     List<HomeHorModel> homeHorModelList;
     HomeHorAdapter homeHorAdapter;
+
+    //////vertical
+    List<HomeVerModel> homeVerModelList;
+    HomeVerAdapter homeVerAdapter;
+
 
     private FragmentHomeBinding binding;
 
@@ -40,7 +47,9 @@ public class HomeFragment extends Fragment {
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
         homeHorizontalRec = root.findViewById(R.id.home_hor_rec);
+        homeVerticalRec = root.findViewById(R.id.home_ver_rec);
 
+        ////Horizontal Recyclerview
         homeHorModelList = new ArrayList<>();
 
         homeHorModelList.add(new HomeHorModel(R.drawable.pizza,"Pizza"));
@@ -54,6 +63,19 @@ public class HomeFragment extends Fragment {
         homeHorizontalRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
         homeHorizontalRec.setHasFixedSize(true);
         homeHorizontalRec.setNestedScrollingEnabled(false);
+
+        //////Vertical Recyclerview
+        homeVerModelList = new ArrayList<>();
+
+        homeVerModelList.add(new HomeVerModel(R.drawable.pizza1, "Pizza", "7:00 - 23:00", "4.9", "Min - $35"));
+        homeVerModelList.add(new HomeVerModel(R.drawable.pizza2, "Pizza", "7:00 - 23:00", "4.9", "Min - $35"));
+        homeVerModelList.add(new HomeVerModel(R.drawable.pizza3, "Pizza", "7:00 - 23:00", "4.9", "Min - $35"));
+
+        homeVerAdapter = new HomeVerAdapter(getActivity(),homeVerModelList);
+        homeVerticalRec.setAdapter(homeVerAdapter);
+        homeVerticalRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.VERTICAL,false));
+        homeVerticalRec.setHasFixedSize(true);
+        homeVerticalRec.setNestedScrollingEnabled(false);
         return root;
 
     }
